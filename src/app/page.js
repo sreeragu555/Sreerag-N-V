903019967b91f888e8c19937af3b0557f4ebaca0
Sreerag N V <sreeragu555@gmail.com>
2023-07-05 21:25:50 +0530

    Making responsive

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ const navigation = [
 export default function Home() {
 	return (
 		<div>
-			<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+			<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black to-black">
 			<nav className="my-16 animate-fade-in">
 				<ul className="flex items-center justify-center gap-4">
 					{navigation.map((item) => (
